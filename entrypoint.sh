1a6e5a647d6e47ae04478e9d6eec6193be76b39c
codenonprofits <codenonprofits@users.noreply.github.com>
2017-07-27 00:19:28 -0300

    Add dockerfile + entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+case $1 in
+ runserver)
+  cd /rapidpro; python manage.py runserver 0.0.0.0:8000
+  ;;
+ uwsgi_server)
+  uwsgi  --socket app.sock --wsgi-file temba/wsgi.py --chmod-socket=666
+  ;;
+ supervisor)
+  /usr/bin/supervisord -n -c /rapidpro/supervisor-app.conf
+  ;;
+esac
+
+exec "$@"
