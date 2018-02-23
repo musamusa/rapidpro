d8959968677168500c451bc39e1d60f808cd179d
Darin Chambers <darin.chambers@gmail.com>
2018-02-23 10:15:54 -0500

    added compress line, adjusted gunicorn timeout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo Compressor
+python manage.py compress --extension=.haml --force
+
 echo Starting gunicorn
-gunicorn --workers 3 --bind unix:/rapidpro/rapidpro.sock temba.wsgi:application &
+gunicorn --timeout 90 --workers 3 --bind unix:/rapidpro/rapidpro.sock temba.wsgi:application &
 
 echo Starting celery worker
 celery worker -A temba -B -Q celery,handler,flows,msgs -n rapidpro.celery -l info &
