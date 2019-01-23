5c36126feb41261b915b43798da0a696c00e49d9
teehamaral <contato@teehamaral.com.br>
2019-01-23 12:33:53 -0300

    Fix send email with s3 attachment

--- temba/flows/models.py
+++ temba/flows/models.py
@@ -5286,6 +5286,10 @@ class EmailAction(Action):
 
             if real_media_url and not run.contact.is_test:
                 if settings.DEFAULT_FILE_STORAGE == 'storages.backends.s3boto3.S3Boto3Storage':
+
+                    if settings.AWS_BUCKET_DOMAIN not in real_media_url:
+                        real_media_url = "https://%s/%s" % (settings.AWS_BUCKET_DOMAIN, media_url)
+
                     media_file = Org.get_temporary_file_from_url(real_media_url)
 
                     extension = real_media_url.split('.')[-1]
