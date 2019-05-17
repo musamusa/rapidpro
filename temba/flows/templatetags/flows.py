5972465445779f13fd866f7968b1d202201f87ad
teehamaral <contato@teehamaral.com.br>
2019-05-17 17:58:00 -0300

    Sort keys on trigger words flow start

--- temba/flows/templatetags/flows.py
+++ temba/flows/templatetags/flows.py
@@ -8,6 +8,6 @@ register = template.Library()
 @register.filter
 def list_embedded_data(keyword):
     embedded_data = json.loads(keyword.embedded_data) if keyword.embedded_data else None
-    embedded_data_list = [dict(field=item, value=embedded_data.get(item)) for item in embedded_data.keys()] \
+    embedded_data_list = [dict(field=item, value=embedded_data.get(item)) for item in sorted(embedded_data.keys())] \
         if embedded_data else []
     return embedded_data_list
