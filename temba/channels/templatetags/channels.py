636dc6e3740aa49513226c316a6a7ac636e7ea1c
Teeh Amaral <contato@teehamaral.me>
2019-11-05 15:43:42 -0300

    Enable single quotes and line break on welcome message

--- temba/channels/templatetags/channels.py
+++ temba/channels/templatetags/channels.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter
 def channel_icon(channel):
     return channel.get_type().icon
+
+
+@register.filter
+def adapt_for_widget(text):
+    return text.replace("'", "\\'").replace("\n", "\\n").replace("\r", "\\r")
